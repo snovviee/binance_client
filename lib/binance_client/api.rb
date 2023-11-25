3437a4724cb21c8b6190947ce6eb006d13e97daa
snovviee <snovviee1@gmail.com>
2023-11-25 21:43:47 +0200

    1

--- lib/binance_client/api.rb
+++ lib/binance_client/api.rb
@@ -121,7 +121,7 @@ module BinanceClient
       params = {
         symbol: symbol, side: side.to_s.upcase, type: 'STOP_MARKET', stopPrice: stop_price, closePosition: true
       }
-      params.merge!(goodTillDate: timestamp + 900000, timeInForce: 'GTD') if goodTillDate
+      params.merge!(goodTillDate: timestamp + 1500000, timeInForce: 'GTD') if goodTillDate
 
       place_order(params)
     end
